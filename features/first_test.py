562adcdf519a65780ef8c28b2eb9c66738fcb393
chizuchizu <ochimasumi@gmail.com>
2020-11-14 10:06:57 +0900

    特徴量管理

--- features/first_test.py
+++ features/first_test.py
@@ -1,4 +1,4 @@
-from features.base import Feature, generate_features
+from features.base import Feature, generate_features, create_memo
 from src.pre_fun import base_data
 
 Feature.dir = "../features_data"
@@ -8,6 +8,7 @@ Feature.data = base_data()
 class Cost(Feature):
     def create_features(self):
         self.data["cost"] = self.data["gross_weight"] * self.data["freight_cost"]
+        create_memo("cost", "実際にかかった費用")
 
 
 # class Load(Feature):
